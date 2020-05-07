e29b11308d2f6e1e98f36d4601830845bc3e14f7
Constance  Xu <cxu16@stevens.edu>
2020-05-07 16:21:10 -0400

    added cruise control template

---
+++ cruisecontrol.java
@@ -0,0 +1,40 @@
+public void write_to_log(){
+    //TO DO
+}
+
+public void log_in(){
+    //TO DO
+}
+
+public float get_speed(){
+    //TO DO
+}
+
+public boolean is_cruise_control_on(){
+    //TO DO
+}
+
+public float decrement_speed(){
+    //TO DO
+}
+
+public float increment_speed(){
+    //TO DO
+}
+
+//Originally named brake()
+public boolean is_brake_pressed(){
+    //TO DO
+}
+
+public static void main(){
+    //some shitty UI
+    //we need to create some sort of UI
+    //JAVA swing
+    //as each command goes in, writing to a log
+    //terminal interface
+    //while (1) loop that will continually take input until the user exits out
+    //take input using a buffer reader
+
+
+}
